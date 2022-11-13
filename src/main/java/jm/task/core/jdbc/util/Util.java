636ecd1f4c6956111743ff944f8e58b4e181482f
konruvikt <114674272+konruvikt@users.noreply.github.com>
2022-11-13 11:54:54 +0300

    first commit
    Added the first answer

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+
+    private final static String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private final static String USERNAME = "root1405";
+    private final static String PASSWORD = "root";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+}
